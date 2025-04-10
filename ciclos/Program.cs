aa72d815e967014bd32792fe1227487db9328eb8
FrancoCapurro <franco.capurro@hotmail.com>
2025-04-10 18:15:21 -0300

    test

---
+++ ciclos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ciclos
+            // for ,while ,do while
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // vectores
+
+
+            int[] numeros = new int[10];
+            float[] precios = new float[12];
+
+            numeros[0] = 10;
+            
+            int j = numeros[0];
+
+            Console.WriteLine(Saludar("franco"));
+            Console.ReadKey();
+            
+                
+                
+        }
+        static int sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        static string Saludar(string nombre)
+        {
+
+            return "hola" + nombre;
+        }
+    }
+}
---
+++ ejemplo.net/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int float bool char
+            // double decimal, long, short, string, datetime
+
+            int a,b,c;
+
+            Console.WriteLine("Ingrese un numero");
+            a = int.Parse(Console.ReadLine());
+
+            b = 10;
+
+            c = a + b;
+            
+            
+
+            Console.WriteLine("El resultado es:" + c);
+
+            // IF
+
+            if (a == b)
+            {
+                Console.WriteLine("A y B son iguales");
+            } else
+            {
+                Console.WriteLine("Son distintos");
+
+            }
+
+                Console.ReadKey();
+
+            switch (a)
+            {
+                case 0:
+                      break;
+                case 1:
+                      break;
+                case 2:
+                      break;
+                case 3:
+                      break;
+                default:
+                      break;
+            }
+        }
+    }
+}
